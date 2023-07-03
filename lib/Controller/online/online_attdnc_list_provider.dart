6d1689a14824b3a0bae9ee2e29bfd0e7a6fdcf87
Nico-Itorma <nicoitorma38@gmail.com>
2023-07-03 15:38:55 +0800

    v1.0.0+7

--- lib/Controller/online/online_attdnc_list_provider.dart
+++ lib/Controller/online/online_attdnc_list_provider.dart
@@ -1,16 +1,33 @@
-import 'package:qr_attendance_flut/Controller/base_provider.dart';
+import 'package:flutter/material.dart';
 import 'package:qr_attendance_flut/Models/attendance.dart';
 import 'package:qr_attendance_flut/Repository/online_repo.dart/online_attendancelist_repo.dart';
 
 import '../../utils/firebase_helper.dart';
 
-class OnlineAttendanceListProvider extends BaseProvider {
+class OnlineAttendanceListProvider extends ChangeNotifier {
+  List<AttendanceModel> attendanceList = [];
+  List selectedTile = [];
+  bool isLongPress = false;
+
+  setLongPress() {
+    isLongPress = !isLongPress;
+    notifyListeners();
+  }
+
   /// This function fetches attendance data from an online repository and adds it to a list if the user's
   /// email is included in the data.
   getAttendance() async {
     attendanceList = await convertDocsFromFirestore();
     print(attendanceList.map((e) => e.attendanceName));
     notifyListeners();
+  }
+
+  selectTile(AttendanceModel attendanceModel) {
+    selectedTile.add(attendanceModel);
+  }
+
+  removeItemFromSelected(AttendanceModel attendanceModel) {
+    selectedTile.remove(attendanceModel);
   }
 
   joinAttendance(String code) async {
--- lib/Controller/online/separate_shit/provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import '../../../Models/attendance.dart';
-
-class HomepageProv extends ChangeNotifier {
-  List<AttendanceModel> currentList = [];
-
-  updateList(List<AttendanceModel> list) => currentList = list;
-}
